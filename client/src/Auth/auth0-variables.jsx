cbc483f30bf6e99d1ae5e22e979e374bf0ca489b
David <dlum988@gmail.com>
2019-02-25 14:05:20 -0600

    added callback to end of callbackurl

--- client/src/Auth/auth0-variables.jsx
+++ client/src/Auth/auth0-variables.jsx
@@ -2,7 +2,7 @@ const AUTH_CONFIG = {
   domain: 'youwannabet.auth0.com',
   clientId: 'K7r5ykHmTG6iozSuIeZsBEX2LZ5m5NDq',
   // callbackUrl: 'http://localhost:3000/callback',
-  callbackUrl: 'https://polar-brushlands-88901.herokuapp.com/',
+  callbackUrl: 'https://polar-brushlands-88901.herokuapp.com/callback',
   // clientId: '3yUuP72XyjtMvg7kMRpLVjAYfvjIHARe',
   // callbackUrl: 'http://localhost:3000/callback',
 };
