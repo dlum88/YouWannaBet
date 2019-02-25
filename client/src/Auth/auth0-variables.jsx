cdff0b8b040ac5982a02a0c4946c3cfeba54f33c
David <dlum988@gmail.com>
2019-02-25 14:55:13 -0600

    changed clientId in auth0

--- client/src/Auth/auth0-variables.jsx
+++ client/src/Auth/auth0-variables.jsx
@@ -1,9 +1,9 @@
 const AUTH_CONFIG = {
   domain: 'youwannabet.auth0.com',
-  clientId: 'K7r5ykHmTG6iozSuIeZsBEX2LZ5m5NDq',
+  // clientId: 'K7r5ykHmTG6iozSuIeZsBEX2LZ5m5NDq',
   // callbackUrl: 'http://localhost:3000/callback',
+  clientId: '3yUuP72XyjtMvg7kMRpLVjAYfvjIHARe',
   callbackUrl: 'https://polar-brushlands-88901.herokuapp.com/callback',
-  // clientId: '3yUuP72XyjtMvg7kMRpLVjAYfvjIHARe',
   // callbackUrl: 'http://localhost:3000/callback',
 };
 
