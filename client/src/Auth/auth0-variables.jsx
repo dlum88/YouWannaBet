f3f9aa092c81fcdf30996c8a5824d5f87b1df1fd
David <dlum988@gmail.com>
2019-02-25 13:56:52 -0600

    changed auth callbackurl

--- client/src/Auth/auth0-variables.jsx
+++ client/src/Auth/auth0-variables.jsx
@@ -1,7 +1,8 @@
 const AUTH_CONFIG = {
   domain: 'youwannabet.auth0.com',
   clientId: 'K7r5ykHmTG6iozSuIeZsBEX2LZ5m5NDq',
-  callbackUrl: 'http://localhost:3000/callback',
+  // callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://polar-brushlands-88901.herokuapp.com/',
 };
 
 export { AUTH_CONFIG as default };
